602e8af9ac4dc42de7703aa9e45353a9df2cd55b
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-04-21 19:52:44 +0200

    Add encryption/decryption tests

--- test/test_rc4.cpp
+++ test/test_rc4.cpp
@@ -3,6 +3,13 @@
 
 using namespace libcrypt;
 
+static bool compare_buffers(const std::vector<uint8_t>& b1, const std::vector<uint8_t>& b2) {
+    if (b1.size() != b2.size())
+        return false;
+
+    return std::equal(b1.begin(), b1.end(), b2.begin());
+}
+
 TEST(rc4, key_setting) {
     rc4 rc4;
 
@@ -40,4 +47,64 @@ TEST(rc4, iv_setting) {
     
     rc4.set_iv(132);
     EXPECT_TRUE(rc4.get_iv() == 132);
+}
+
+TEST(rc4, buffer_encrypt_decrypt_ok) {
+    rc4 rc4;
+
+    std::vector<uint8_t> v1{
+        0x03, 0x48, 0xA7, 0x8, 0x54, 0xBA, 0x99, 0xF7, 0x73, 0x11
+    };
+    std::vector<uint8_t> v2 = v1;
+
+    rc4.set_key("testing");
+    rc4.set_iv(91);
+
+    rc4.encrypt_buffer(v2);
+    rc4.decrypt_buffer(v2);
+
+    EXPECT_TRUE(compare_buffers(v1, v2));
+
+    rc4.encrypt_buffer(v2);
+    rc4.decrypt_buffer(v2);
+
+    EXPECT_TRUE(compare_buffers(v1, v2));
+}
+
+TEST(rc4, buffer_encrypt_decrypt_fail_wrong_iv) {
+    rc4 rc4;
+
+    std::vector<uint8_t> v1{
+        0x03, 0x48, 0xA7, 0x8, 0x54, 0xBA, 0x99, 0xF7, 0x73, 0x11
+    };
+    std::vector<uint8_t> v2 = v1;
+
+    rc4.set_key("testing");
+    rc4.set_iv(91);
+
+    rc4.encrypt_buffer(v2);
+
+    rc4.set_iv(92);
+    rc4.decrypt_buffer(v2);
+
+    EXPECT_FALSE(compare_buffers(v1, v2));
+}
+
+TEST(rc4, buffer_encrypt_decrypt_fail_wrong_key) {
+    rc4 rc4;
+
+    std::vector<uint8_t> v1{
+        0x03, 0x48, 0xA7, 0x8, 0x54, 0xBA, 0x99, 0xF7, 0x73, 0x11
+    };
+    std::vector<uint8_t> v2 = v1;
+
+    rc4.set_key("testing");
+    rc4.set_iv(91);
+
+    rc4.encrypt_buffer(v2);
+
+    rc4.set_key("testing2");
+    rc4.decrypt_buffer(v2);
+
+    EXPECT_FALSE(compare_buffers(v1, v2));
 }
