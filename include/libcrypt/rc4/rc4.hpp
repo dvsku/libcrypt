6f7a894193eab81c289c4e9994f5c419b7c16118
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-04-21 19:52:31 +0200

    Implement encryption/decryption

--- include/libcrypt/rc4/rc4.hpp
+++ include/libcrypt/rc4/rc4.hpp
@@ -3,11 +3,14 @@
 #include "libcrypt/misc/crypt_result.hpp"
 
 #include <filesystem>
+#include <vector>
+#include <cstdint>
 
 namespace libcrypt {
     class rc4 {
     public:
         using file_path_t = std::filesystem::path;
+        using buffer_t    = std::vector<uint8_t>;
 
     public:
         rc4();
@@ -46,6 +49,38 @@ namespace libcrypt {
         // Create a key file
         crypt_result create_key_file(const file_path_t& file, const std::string& key, uint8_t iv);
 
+        // Preforms encryption on the input file and saves the encrypted data to
+        // the output file.
+        // Encryption is skipped if file is encrypted.
+        // If output is empty, result will be saved to input.
+        crypt_result encrypt_file(const file_path_t& input, const file_path_t& output = "");
+
+        // Preforms encryption on the input file and saves the encrypted data to
+        // the out buffer.
+        // Encryption is skipped if file is encrypted.
+        crypt_result encrypt_file(const file_path_t& input, buffer_t& out);
+
+        // Preforms encryption on the buffer.
+        // Encryption is skipped if data is encrypted.
+        // Buffer content and size will be modified.
+        crypt_result encrypt_buffer(buffer_t& buffer);
+
+        // Preforms decryption on the input file and saves the data to
+        // the output file.
+        // Decryption is skipped if file is not encrypted.
+        // If output is empty, result will be saved to input.
+        crypt_result decrypt_file(const file_path_t& input, const file_path_t& output = "");
+
+        // Preforms decryption on the input file and saves the data to
+        // the out buffer.
+        // Decryption is skipped if file is not encrypted.
+        crypt_result decrypt_file(const file_path_t& input, buffer_t& out);
+
+        // Preforms decryption on the buffer.
+        // Decryption is skipped if file is not encrypted.
+        // Buffer content and size will be modified.
+        crypt_result decrypt_buffer(buffer_t& buffer);
+
     private:
         bool        m_initialized;
         uint32_t    m_index_A;
@@ -54,5 +89,11 @@ namespace libcrypt {
         uint8_t     m_box[256];
         std::string m_key;
         uint8_t     m_iv;
+
+    private:
+        void generate_box();
+
+        crypt_result crypt(rc4::buffer_t& buffer);
+        crypt_result crypt(uint8_t* ptr, size_t size, size_t offset = 0, bool keep_box = false);
     };
 }
--- source/rc4/rc4.cpp
+++ source/rc4/rc4.cpp
@@ -9,9 +9,17 @@ using namespace libcrypt;
 // INTERNAL
 
 static const uint32_t KEY_FILE_MAGIC   = 0x20464B44;
+static const uint32_t ENCRYPTION_MAGIC = 0x4B535644;
 
 static std::string internal_parse_key(const char* key, size_t size);
 static std::string internal_hex_string_to_string(const std::string& hex_string);
+
+static void internal_swap(uint8_t* buffer, uint32_t i, uint32_t j);
+static crypt_result internal_write(std::filesystem::path& output, const rc4::buffer_t& buffer);
+
+static bool internal_has_magic(const rc4::buffer_t& buffer);
+static void internal_write_magic(rc4::buffer_t& buffer);
+static void internal_remove_magic(rc4::buffer_t& buffer);
 
 ///////////////////////////////////////////////////////////////////////////////
 // PUBLIC
@@ -24,12 +32,7 @@ rc4::rc4() {
 }
 
 void rc4::reset() {
-    m_initialized     = false;
-    m_index_A         = 0U;
-    m_index_B         = 0U;
-    m_previous_offset = 0U;
-
-    std::memset(m_box, 0, sizeof(m_box));
+    m_initialized = false;
 }
 
 void rc4::set_key(const char* key, size_t size) {
@@ -132,6 +135,178 @@ crypt_result rc4::create_key_file(const file_path_t& file, const std::string& ke
     return result;
 }
 
+crypt_result rc4::encrypt_file(const file_path_t& input, const file_path_t& output) {
+    buffer_t buffer;
+    
+    auto result = encrypt_file(input, buffer);
+    if (!result)
+        return result;
+
+    file_path_t output_path = output == "" ? input : output;
+    return internal_write(output_path, buffer);
+}
+
+crypt_result rc4::encrypt_file(const file_path_t& input, buffer_t& out) {
+    crypt_result result;
+
+    if (!std::filesystem::exists(input)) {
+        result.message = "Input file not found.";
+        return result;
+    }
+
+    std::ifstream fin(input, std::ios::binary | std::ios::in | std::ios::ate);
+    if (!fin.is_open()) {
+        result.message = "Failed to open input file.";
+        return result;
+    }
+
+    size_t size = (size_t)fin.tellg();
+    out.resize(size);
+    fin.seekg(0, std::ios::beg);
+
+    if (!fin.read((char*)out.data(), size)) {
+        result.message = "Failed to read input file.";
+        return result;
+    }
+
+    fin.close();
+
+    return encrypt_buffer(out);
+}
+
+crypt_result rc4::encrypt_buffer(buffer_t& buffer) {
+    crypt_result result;
+
+    if (internal_has_magic(buffer)) {
+        result.message = "Buffer is already encrypted.";
+        return result;
+    }
+
+    result = crypt(buffer);
+    if (!result)
+        return result;
+
+    internal_write_magic(buffer);
+
+    result.success = true;
+    return result;
+}
+
+crypt_result rc4::decrypt_file(const file_path_t& input, const file_path_t& output) {
+    buffer_t buffer;
+
+    auto result = decrypt_file(input, buffer);
+    if (!result)
+        return result;
+
+    file_path_t output_path = output == "" ? input : output;
+    return internal_write(output_path, buffer);
+}
+
+crypt_result rc4::decrypt_file(const file_path_t& input, buffer_t& out) {
+    crypt_result result;
+
+    if (!std::filesystem::exists(input)) {
+        result.message = "Input file not found.";
+        return result;
+    }
+
+    std::ifstream fin(input, std::ios::binary | std::ios::in | std::ios::ate);
+    if (!fin.is_open()) {
+        result.message = "Failed to open input file.";
+        return result;
+    }
+
+    size_t size = (size_t)fin.tellg();
+    out.resize(size);
+    fin.seekg(0, std::ios::beg);
+
+    if (!fin.read((char*)out.data(), size)) {
+        result.message = "Failed to read input file.";
+        return result;
+    }
+
+    fin.close();
+
+    return decrypt_buffer(out);
+}
+
+crypt_result rc4::decrypt_buffer(buffer_t& buffer) {
+    crypt_result result;
+
+    if (!internal_has_magic(buffer)) {
+        result.message = "Not encrypted.";
+        return result;
+    }
+
+    internal_remove_magic(buffer);
+    return crypt(buffer);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// PRIVATE
+
+void rc4::generate_box() {
+    m_index_A         = 0;
+    m_index_B         = 0;
+    m_previous_offset = 0;
+
+    for (uint32_t i = 0; i < 256; i++) {
+        m_box[i] = (uint8_t)(m_iv ^ 0xFF);
+
+        m_iv = m_iv == 0xFF ? 0x00 : m_iv + 1;
+    }
+
+    uint8_t mod = m_key.size() <= 0xFF ? (uint8_t)m_key.size() : 0xFF;
+
+    uint32_t j = 0;
+    for (uint32_t i = 0; i < 256; i++) {
+        j += m_box[i];
+        j += m_key[i % mod];
+        j %= 256;
+
+        internal_swap(m_box, i, j);
+    }
+
+    m_initialized = true;
+}
+
+crypt_result rc4::crypt(rc4::buffer_t& buffer) {
+    return crypt(buffer.data(), buffer.size(), false);
+}
+
+crypt_result rc4::crypt(uint8_t* ptr, size_t size, size_t offset, bool keep_box) {
+    crypt_result result;
+
+    if (!m_initialized)
+        generate_box();
+
+    if (keep_box && offset != m_previous_offset) {
+        generate_box();
+
+        for (uint64_t i = 0; i < offset; i++) {
+            m_index_A = (m_index_A + 1) % 256;
+            m_index_B = (m_index_B + m_box[m_index_A]) % 256;
+            internal_swap(m_box, m_index_A, m_index_B);
+        }
+    }
+
+    for (uint64_t i = 0; i < size; i++) {
+        m_index_A = (m_index_A + 1) % 256;
+        m_index_B = (m_index_B + m_box[m_index_A]) % 256;
+        internal_swap(m_box, m_index_A, m_index_B);
+        ptr[i] = m_box[(m_box[m_index_A] + m_box[m_index_B]) % 256] ^ ptr[i];
+    }
+
+    if (!keep_box)
+        m_initialized = false;
+    else
+        m_previous_offset += size;
+
+    result.success = true;
+    return result;
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // INTERNAL
 
@@ -153,4 +328,60 @@ std::string internal_hex_string_to_string(const std::string& hex_string) {
         ss << static_cast<char>(std::stoi(hex_data.substr(i, 2), nullptr, 16));
 
     return ss.str();
+}
+
+void internal_swap(uint8_t* buffer, uint32_t i, uint32_t j) {
+    uint8_t temp = buffer[i];
+    buffer[i]    = buffer[j];
+    buffer[j]    = temp;
+}
+
+crypt_result internal_write(std::filesystem::path& output, const rc4::buffer_t& buffer) {
+    crypt_result result;
+
+    std::filesystem::path dir_path(output);
+    dir_path.remove_filename();
+
+    if (!std::filesystem::is_directory(dir_path)) {
+        std::filesystem::create_directories(dir_path);
+        std::filesystem::permissions(dir_path, std::filesystem::perms::all);
+    }
+
+    std::ofstream out(output, std::ios::binary | std::ios::out);
+
+    if (!out.is_open()) {
+        result.message = "Failed to open output file.";
+        return result;
+    }
+
+    out.write((char*)buffer.data(), buffer.size());
+    out.close();
+
+    result.success = true;
+    return result;
+}
+
+bool internal_has_magic(const rc4::buffer_t& buffer) {
+    if (buffer.size() < 4)
+        return false;
+
+    uint32_t magic = 0;
+    std::memcpy(&magic, buffer.data(), 4);
+
+    return magic == ENCRYPTION_MAGIC;
+}
+
+void internal_write_magic(rc4::buffer_t& buffer) {
+    if (internal_has_magic(buffer)) return;
+
+    buffer.resize(buffer.size() + 4);
+    std::memmove(buffer.data() + 4, buffer.data(), buffer.size() - 4);
+    std::memcpy(buffer.data(), &ENCRYPTION_MAGIC, 4);
+}
+
+void internal_remove_magic(rc4::buffer_t& buffer) {
+    if (!internal_has_magic(buffer)) return;
+
+    std::memmove(buffer.data(), buffer.data() + 4, buffer.size() - 4);
+    buffer.resize(buffer.size() - 4);
 }
